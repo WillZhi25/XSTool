516f96773a9d2d3a8c6149f5264e0e982ab3a8b4
mihawk <245666044@qq.com>
2015-04-29 18:45:06 +0800

    first commit

---
+++ XSTool/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XSTool
+//
+//  Created by 植梧培 on 15/4/29.
+//  Copyright (c) 2015年 机智的新手( http://zhiwupei.sinaapp.com ). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ XSTool/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  XSTool
+//
+//  Created by 植梧培 on 15/4/29.
+//  Copyright (c) 2015年 机智的新手( http://zhiwupei.sinaapp.com ). All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
